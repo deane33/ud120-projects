017fa5f553c7a65a59060a4bbb59e8de20f62d8f
Dean E <100791137+deane33@users.noreply.github.com>
2023-10-08 23:42:56 +0000

    just finished svm

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,24 +7,31 @@
     Sara has label 0
     Chris has label 1
 """
+    
 import sys
 from time import time
+sys.path.append("tools/")
 from email_preprocess import preprocess
-
+from sklearn import svm
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-clf - svm.SVC()
-clf.fit(features_train,labels_train)
-print(clf.predict(features_test))
 
 #########################################################
 ### your code goes here ###
-#https://scikit-learn.org/stable/modules/svm.html
+# clf = svm.SVC(kernel='linear')
+# t0 = time()
+# clf.fit(features_train,labels_train)
+# print("Fit Time:", round(time()-t0, 3), "s")
 
+# t0 = time()
+# clf.predict(features_test)
+# print("Predict Time:", round(time()-t0, 3), "s")
+
+# print(clf.score(features_test,labels_test))
 
 #########################################################
 
@@ -35,7 +42,18 @@ But the Code provided in Quiz has an Indexing issue
 The Code Below solves that issue, So use this one
 '''
 
-# features_train = features_train[:int(len(features_train)/100)]
-# labels_train = labels_train[:int(len(labels_train)/100)]
+#features_train = features_train[:int(len(features_train)/100)]
+#labels_train = labels_train[:int(len(labels_train)/100)]
+
+clf = svm.SVC(kernel='rbf',C=10000)
+t0 = time()
+clf.fit(features_train,labels_train)
+print("Fit Time:", round(time()-t0, 3), "s")
+
+t0 = time()
+clf.predict(features_test)
+print("Predict Time:", round(time()-t0, 3), "s")
+
+print(clf.score(features_test,labels_test))
 
 #########################################################
